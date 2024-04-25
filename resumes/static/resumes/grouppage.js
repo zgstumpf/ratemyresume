d7951816e0d9f8484e2e9c60877217937680949d
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-04-25 11:26:51 -0500

    removed requests and invites from group page

--- resumes/static/resumes/grouppage.js
+++ resumes/static/resumes/grouppage.js
@@ -8,22 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
         $("#members-data").slideToggle("fast");
     });
 
-    $("#join-request-header").click(function(){
-        $("#join-request-data").slideToggle("fast");
-    });
-
-    $("#join-request-history-header").click(function(){
-        $("#join-request-history-data").slideToggle("fast");
-    });
-
-
-    $("#invitation-header").click(function(){
-        $("#invitation-data").slideToggle("fast");
-    });
-
-    $("#invitation-history-header").click(function(){
-        $("#invitation-history-data").slideToggle("fast");
-    });
 
     $("#send-request-form").submit(function(){
         event.preventDefault();
