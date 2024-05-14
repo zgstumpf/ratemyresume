fe8dfa56dcabbe02b455bf1380d2cd100e242c84
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-05-14 19:15:59 -0400

    fixed code repeating

--- resumes/static/resumes/grouppage.js
+++ resumes/static/resumes/grouppage.js
@@ -30,28 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    // Load preview images of resumes
-    var resume_id_divs = $('.resume-id')
-    resume_id_divs.each(function() {
-        var $this = $(this)
-        $.ajax({
-            type: 'GET',
-            url: $this.data('url'),
-            crossDomain: false,
-            success: function (response) {
-                $this.next('.resume-image-skeleton').html(`<img src="${response.image}" style="max-width: 100%; max-height: ;">`);
-                $this.next('.resume-image-skeleton').css("animation", "none");
-            },
-            error: function (response) {
-                console.error(response.responseJSON.error)
-            }
-        });
-    })
-
-
-    // <div class="resume-card" data-detailsUrl="{% url 'details' resume.id %}">
-    $('.resume-card').click(function(){
-        window.location.href = $(this).data('detailsurl')
+    $('.resume-card').each((_, resumeCard) => {
+        addJavaScriptFunctionality(resumeCard)
     })
 
 
