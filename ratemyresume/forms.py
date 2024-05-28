adee9d54e085c8468b1e8c7e63706f8c21232768
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-05-28 01:18:45 -0400

    enforce stronger passwords

--- ratemyresume/forms.py
+++ ratemyresume/forms.py
@@ -1,17 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
 
 class SignUpForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+        validators=[validate_password],
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'password']
-
-# class LogInForm(forms.ModelForm):
-#     password = forms.CharField(widget=forms.PasswordInput())
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'password']
+        fields = ["username", "password"]
